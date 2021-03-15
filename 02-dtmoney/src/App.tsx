a7635f6f77a0876649b26f2778363987b517b19b
thalesousa <thalestjsb@gmail.com>
2021-03-15 10:18:45 -0300

    Mod02: Criando estilos globais

--- 02-dtmoney/src/App.tsx
+++ 02-dtmoney/src/App.tsx
@@ -1,21 +1,11 @@
 import React from 'react';
-import styled from 'styled-components';
+import { GlobalStyle } from './styles/global';
 
-const Title = styled.h1`
-  color: red;
-  font-size: 64px;
-
-  button{
-    background: green;
-  }
-`
 export function App() {
   return (
     <div className="App">
-      <Title>
-        Hello World
-        <button>Salvar</button>
-      </Title>
+      <h1>Hello World</h1>
+      <GlobalStyle />
     </div>
   );
 }
