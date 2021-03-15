587e55a9114c678061aabc344be5cd9a82367042
thalesousa <thalestjsb@gmail.com>
2021-03-15 10:00:21 -0300

    Mod02: Instalando Styled Components

--- 02-dtmoney/src/App.tsx
+++ 02-dtmoney/src/App.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
 
+const Title = styled.h1`
+  color: red;
+  font-size: 64px;
+
+  button{
+    background: green;
+  }
+`
 export function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+      <Title>
+        Hello World
+        <button>Salvar</button>
+      </Title>
+    </div>
   );
 }
