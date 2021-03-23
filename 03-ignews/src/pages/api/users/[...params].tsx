d0d7b558eb6420d81a1e6d8c7982e9e76540c5b7
thalesousa <thalestjsb@gmail.com>
2021-03-23 15:39:48 -0300

    Mod03: Parametrização nas rotas

--- 03-ignews/src/pages/api/users/[...params].tsx
+++ 03-ignews/src/pages/api/users/[...params].tsx
@@ -1,10 +1,12 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default (request: NextApiRequest, response: NextApiResponse) => {
+  console.log(request.query);
+
   const users = [
     { id: 1, name: 'Thales'},
-    { id: 1, name: 'John Doe'},
-    { id: 1, name: 'Ciclano'},
+    { id: 2, name: 'John Doe'},
+    { id: 3, name: 'Ciclano'},
   ]
 
   return response.json(users)
---
+++ 03-ignews/src/pages/api/users/index.ts
@@ -0,0 +1,11 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default (request: NextApiRequest, response: NextApiResponse) => {
+  const users = [
+    { id: 1, name: 'Thales'},
+    { id: 2, name: 'John Doe'},
+    { id: 3, name: 'Ciclano'},
+  ]
+
+  return response.json(users)
+}
