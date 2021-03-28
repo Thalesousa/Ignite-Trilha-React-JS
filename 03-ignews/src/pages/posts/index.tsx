d111f82415fa4fbe19f08e4548e67e00b0c23bdb
thalesousa <thalestjsb@gmail.com>
2021-03-28 15:21:29 -0300

    Mod03: Listando posts em tela

--- 03-ignews/src/pages/posts/index.tsx
+++ 03-ignews/src/pages/posts/index.tsx
@@ -2,9 +2,22 @@ import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import { getPrismicClient } from '../../services/prismic';
 import Prismic from '@prismicio/client';
+import { RichText } from 'prismic-dom';
+
 import styles from './styles.module.scss';
 
-export default function Posts() {
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+}
+
+interface PostsProps {
+  posts: Post[]
+}
+
+export default function Posts({ posts }: PostsProps) {
   return (
     <>
       <Head>
@@ -13,32 +26,13 @@ export default function Posts() {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="#">
-            <time>12 de março de 2021</time>
-            <strong>Title post em Lorem ipsum dolor sit amet.</strong>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident 
-              rerum quos expedita suscipit quis mollitia!
-            </p>
-          </a>
-
-          <a href="#">
-            <time>12 de março de 2021</time>
-            <strong>Title post em Lorem ipsum dolor sit amet.</strong>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident 
-              rerum quos expedita suscipit quis mollitia!
-            </p>
-          </a>
-
-          <a href="#">
-            <time>12 de março de 2021</time>
-            <strong>Title post em Lorem ipsum dolor sit amet.</strong>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident 
-              rerum quos expedita suscipit quis mollitia!
-            </p>
-          </a>
+          { posts.map(post => (
+            <a key={ post.slug } href="#">
+              <time>{ post.updatedAt }</time>
+              <strong>{ post.title }</strong>
+              <p>{ post.excerpt }</p>
+            </a>
+          ))}
         </div>
       </main>
     </>
@@ -55,9 +49,20 @@ export const getStaticProps: GetStaticProps = async () => {
     pageSize: 100,
   })
 
-  console.log(response)
+  const posts = response.results.map(post => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric',
+      })
+    }
+  })
 
   return {
-    props: {}
+    props: {posts}
   }
 }
