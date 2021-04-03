e0926aa8f7883bc90932429d785aa3d5f924e7cc
thalesousa <thalestjsb@gmail.com>
2021-04-03 16:59:08 -0300

    Mod04: Header responsivo

--- 04-dashgo/src/components/Header/Profile.tsx
+++ 04-dashgo/src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true}: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right" >
-        <Text>Thales José Sousa Bezerra</Text>
-        <Text color="gray.300" fontSize="small">
-          thalesousa@gmail.com
-        </Text>
-      </Box>
+      { showProfileData && (
+        <Box mr="4" textAlign="right" >
+          <Text>Thales José Sousa Bezerra</Text>
+          <Text color="gray.300" fontSize="small">
+            thalesousa@gmail.com
+          </Text>
+        </Box>
+      )}
       <Avatar size="md" name="Thales Sousa" src="https://github.com/thalesousa.png" />
     </Flex>
   );
