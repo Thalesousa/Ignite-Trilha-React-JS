fe57182622255d7aa1b4f238af96637e1cf626c3
thalesousa <thalestjsb@gmail.com>
2021-05-18 16:38:55 -0300

    Criando hook de permissão

--- 05-next-auth/next-auth/src/contexts/AuthContext.tsx
+++ 05-next-auth/next-auth/src/contexts/AuthContext.tsx
@@ -5,7 +5,7 @@ import Router from 'next/router';
 
 type User = {
   email: string;
-  permission: string[];
+  permissions: string[];
   roles: string[];
 }
 
@@ -42,9 +42,9 @@ export function AuthProvider({ children }: AuthProviderProps) {
 
     if (token) {
       api.get('/me').then(response => {
-        const { email, permission, roles } = response.data
+        const { email, permissions, roles } = response.data
 
-        setUser({ email, permission, roles})
+        setUser({ email, permissions, roles})
       })
       .catch(() => {
         signOut()
@@ -59,7 +59,7 @@ export function AuthProvider({ children }: AuthProviderProps) {
         password,
       })
 
-      const { token, refreshToken , permission, roles} = response.data;
+      const { token, refreshToken , permissions, roles} = response.data;
 
       setCookie(undefined, 'nextauth.token', token, {
         maxAge: 60 * 60 * 24 * 30, // 30 days
@@ -72,7 +72,7 @@ export function AuthProvider({ children }: AuthProviderProps) {
 
       setUser({
         email,
-        permission,
+        permissions,
         roles,
       })
 
