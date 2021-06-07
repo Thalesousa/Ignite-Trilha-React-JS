e44754495706aa716bcaac8f7113bfaf9eb41c03
thalesousa <thalestjsb@gmail.com>
2021-06-07 15:46:51 -0300

    Virtualização

--- 06-perfreact/src/components/SearchResults.tsx
+++ 06-perfreact/src/components/SearchResults.tsx
@@ -1,7 +1,7 @@
 import { ProductItem } from "./ProductItem"
-import { useMemo } from "react";
+import { List, ListRowRenderer } from 'react-virtualized';
 
-type Product ={
+type Product = {
   id: number;
   price: number;
   title: string;
@@ -14,19 +14,28 @@ type SearchResultsProps = {
   onAddToWishList: (id: number) => void;
 }
 
-export function SearchResults({ totalPrice, results, onAddToWishList }: SearchResultsProps ) {
-  return(
+export function SearchResults({ totalPrice, results, onAddToWishList }: SearchResultsProps) {
+  const rowRenderer: ListRowRenderer = ({ index, key, style }) => {
+    return (
+      <div key={key} style={style}>
+        <ProductItem
+          product={results[index]}
+          onAddToWishList={onAddToWishList}
+        />
+      </div>
+    )
+  }
+  return (
     <div>
       <h2>{totalPrice}</h2>
-      {results.map(product => {
-        return(
-          <ProductItem 
-            product={product}
-            onAddToWishList={onAddToWishList}
-            key={product.id} 
-          />
-        )
-      })}
+      <List
+        height={300}
+        rowHeight={30}
+        width={900}
+        overscanRowCount={5}
+        rowCount={results.length}
+        rowRenderer={rowRenderer}
+      />
     </div>
   )
 }
